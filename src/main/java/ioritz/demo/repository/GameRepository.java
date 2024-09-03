4af07a2e3d2014759417bdb84f12cc7577a3fab0
ioritz <ioritzurkiri2002@gmail.com>
2024-09-03 19:39:23 +0200

    Find game by id works

--- src/main/java/ioritz/demo/repository/GameRepository.java
+++ src/main/java/ioritz/demo/repository/GameRepository.java
@@ -4,5 +4,5 @@ import ioritz.demo.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-
+    public Game findByName(String name);
 }
